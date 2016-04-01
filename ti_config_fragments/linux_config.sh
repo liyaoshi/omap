b82f4890f9ec1ebe77fddea48b6ac7abb3647e6c
Praneeth Bajjuri <praneeth@ti.com>
2016-04-01 16:04:04 -0500

    ti_config_fragments: dra7xx: Add wrapper scripts for Linux merge-config
    Adds convinient wrapper scripts for merging linux config fragments.
    
    Change-Id: I8eb3fe5dc327fc00d2f48e59c11ce451ad956675
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

---
+++ ti_config_fragments/linux_config.sh
@@ -0,0 +1,9 @@
+export ARCH=arm
+ 
+./scripts/kconfig/merge_config.sh \
+arch/arm/configs/omap2plus_defconfig \
+ti_config_fragments/baseport.cfg \
+ti_config_fragments/audio_display.cfg \
+ti_config_fragments/ipc.cfg \
+ti_config_fragments/connectivity.cfg \
+ti_config_fragments/dra7_only.cfg
