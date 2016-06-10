eaa1bb698917b634c89777ffc9dcfbb1219baa18
Praneeth Bajjuri <praneeth@ti.com>
2016-06-10 16:38:22 -0500

    ti_config_fragments: android_config.sh: Fix ordering of cfg files
    commit c40bc5ff4bf6 "ti_config_fragments/defconfig_map.txt: Fix ordering of cfg files"
    introduces the correct order to be used.
    
    This patch fixes the android config script accordingly.
    
    Note: Right order has to be used to get the correct dependencies enabled.
    
    Change-Id: Ib76731d80d616fd9029b7431ae474e2df96f7fca
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

--- ti_config_fragments/android_config.sh
+++ ti_config_fragments/android_config.sh
@@ -2,12 +2,14 @@ export ARCH=arm
 
 ./scripts/kconfig/merge_config.sh \
 arch/arm/configs/multi_v7_defconfig \
+ti_config_fragments/multi_v7_prune.cfg \
+ti_config_fragments/omap_soc.cfg \
+ti_config_fragments/dra7_only.cfg \
+ti_config_fragments/lpae.cfg \
 ti_config_fragments/baseport.cfg \
-ti_config_fragments/audio_display.cfg \
 ti_config_fragments/ipc.cfg \
 ti_config_fragments/connectivity.cfg \
-ti_config_fragments/multi_v7_prune.cfg \
-ti_config_fragments/dra7_only.cfg \
+ti_config_fragments/audio_display.cfg \
 android/configs/android-base.cfg \
 android/configs/android-recommended.cfg \
 ti_config_fragments/auto.cfg \
