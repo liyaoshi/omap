b94ec4baf6e44d99e42e90f60b65708bc4497f13
HuangXiongwei <Xiongwei.huang@visteon.com>
2017-05-27 12:28:07 +0800

    Add gpu in hwmon

--- arch/arm/mach-omap2/omap_hwmod_54xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_54xx_data.c
@@ -894,6 +894,42 @@ static struct omap_hwmod omap54xx_gpmc_hwmod = {
 };
 
 /*
+ * 'gpu' class
+ * 2d/3d graphics accelerator
+ */
+
+static struct omap_hwmod_class_sysconfig omap54xx_gpu_sysc = {
+	.rev_offs	= 0x0000,
+	.sysc_offs	= 0x0010,
+	.sysc_flags	= (SYSC_HAS_MIDLEMODE | SYSC_HAS_SIDLEMODE),
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |
+			   SIDLE_SMART_WKUP | MSTANDBY_FORCE | MSTANDBY_NO |
+			   MSTANDBY_SMART | MSTANDBY_SMART_WKUP),
+	.sysc_fields	= &omap_hwmod_sysc_type2,
+};
+
+static struct omap_hwmod_class omap54xx_gpu_hwmod_class = {
+	.name	= "gpu",
+	.sysc	= &omap54xx_gpu_sysc,
+};
+
+/* gpu */
+
+static struct omap_hwmod omap54xx_gpu_hwmod = {
+	.name		= "gpu",
+	.class		= &omap54xx_gpu_hwmod_class,
+	.clkdm_name	= "gpu_clkdm",
+	.main_clk	= "gpu_core_gclk_mux",
+	.prcm = {
+		.omap4 = {
+			.clkctrl_offs = OMAP54XX_CM_GPU_GPU_CLKCTRL_OFFSET,
+			.context_offs = OMAP54XX_RM_GPU_GPU_CONTEXT_OFFSET,
+			.modulemode   = MODULEMODE_SWCTRL,
+		},
+	},
+};
+
+/*
  * 'i2c' class
  * multimaster high-speed i2c controller
  */
@@ -2532,6 +2568,13 @@ static struct omap_hwmod_ocp_if omap54xx_l3_main_2__gpmc = {
 	.user		= OCP_USER_MPU | OCP_USER_SDMA,
 };
 
+/* l3_main_2 -> gpu */
+static struct omap_hwmod_ocp_if omap54xx_l3_main_2__gpu = {
+	.master		= &omap54xx_l3_main_2_hwmod,
+	.slave		= &omap54xx_gpu_hwmod,
+	.clk		= "gpu_l3_iclk",
+	.user		= OCP_USER_MPU | OCP_USER_SDMA,
+};
 /* l4_per -> i2c1 */
 static struct omap_hwmod_ocp_if omap54xx_l4_per__i2c1 = {
 	.master		= &omap54xx_l4_per_hwmod,
@@ -2924,6 +2967,7 @@ static struct omap_hwmod_ocp_if *omap54xx_hwmod_ocp_ifs[] __initdata = {
 	&omap54xx_l4_per__gpio7,
 	&omap54xx_l4_per__gpio8,
 	&omap54xx_l3_main_2__gpmc,
+	&omap54xx_l3_main_2__gpu,
 	&omap54xx_l4_per__i2c1,
 	&omap54xx_l4_per__i2c2,
 	&omap54xx_l4_per__i2c3,
